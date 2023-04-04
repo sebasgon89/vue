3ae8944e349d8e4dfcf7740e9bc7a7e3cfb4c449
sebasgon89 <seba3789@gmail.com>
2023-04-04 17:47:49 -0300

    Fixed it, using Vue 3 src and the #

---
+++ percipio_gettingStarted/Lesson 01/main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: "#the_food_app",
+  data: {
+    title: 'The food app',
+    subtitle: 'This app is about food',
+    food: [
+      {name: 'apple', description: 'Reeed', src:"./images/apples.jpg"}
+    ]
+  }
+});
