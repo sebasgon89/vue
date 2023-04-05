3e96235e09386330d0565e5ab6491d0af3488c56
sebasgon89 <seba3789@gmail.com>
2023-04-05 17:17:29 -0300

    Added styles. FA does not work anymore

--- percipio_gettingStarted/Lesson 01/main.js
+++ percipio_gettingStarted/Lesson 01/main.js
@@ -4,10 +4,10 @@ var app = new Vue({
     title: 'The food app',
     subtitle: 'This app is about food',
     foods: [
-      {name: 'Apple', description: 'Reeed', src:"./images/apple.jpg", perc_carbs: "93"},
-      {name: 'Banana', description: 'Yeeellow', src: "./images/banana.jpg", perc_carbs: "80"},
-      {name: 'Eggs', description: 'Just crack them', src: "./images/eggs.jpg", perc_carbs: "5"},
-      {name: 'Potatoes', description: 'Better fried', src: "./images/potatoes.jpg", perc_carbs: "75"}
+      {name: 'Apple', description: 'Reeed', src:"./images/apple.jpg", perc_carbs: "93", is_fav: true},
+      {name: 'Banana', description: 'Yeeellow', src: "./images/banana.jpg", perc_carbs: "80", is_fav: false},
+      {name: 'Eggs', description: 'Just crack them', src: "./images/eggs.jpg", perc_carbs: "5",is_fav: false},
+      {name: 'Potatoes', description: 'Better fried', src: "./images/potatoes.jpg", perc_carbs: "75", is_fav: false}
     ]
   }
 });
