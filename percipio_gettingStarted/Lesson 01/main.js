a5ee9e0388e1ce1eb644e846607337401011c784
sebasgon89 <seba3789@gmail.com>
2023-04-05 11:56:18 -0300

    Added if

--- percipio_gettingStarted/Lesson 01/main.js
+++ percipio_gettingStarted/Lesson 01/main.js
@@ -4,9 +4,10 @@ var app = new Vue({
     title: 'The food app',
     subtitle: 'This app is about food',
     foods: [
-      {name: 'Apple', description: 'Reeed', src:"./images/apple.jpg"},
-      {name: 'Banana', description: 'Yeeellow', src: "./images/banana.jpg"},
-      {name: 'Eggs', description: 'Just crack them', src: "./images/eggs.jpg"}
+      {name: 'Apple', description: 'Reeed', src:"./images/apple.jpg", perc_carbs: "93"},
+      {name: 'Banana', description: 'Yeeellow', src: "./images/banana.jpg", perc_carbs: "80"},
+      {name: 'Eggs', description: 'Just crack them', src: "./images/eggs.jpg", perc_carbs: "5"},
+      {name: 'Potatoes', description: 'Better fried', src: "./images/potatoes.jpg", perc_carbs: "75"}
     ]
   }
 });
