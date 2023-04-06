693ec98b1fc9bf25ff5c8d48d5b0f83973d2f55e
sebasgon89 <seba3789@gmail.com>
2023-04-06 10:55:18 -0300

    @mouseover works, but console.log not

--- percipio_gettingStarted/Lesson 01/main.js
+++ percipio_gettingStarted/Lesson 01/main.js
@@ -8,6 +8,6 @@ var app = new Vue({
       {name: 'Banana', description: 'Yeeellow', src: "./images/banana.jpg", perc_carbs: "80", is_fav: false},
       {name: 'Eggs', description: 'Just crack them', src: "./images/eggs.jpg", perc_carbs: "5",is_fav: true},
       {name: 'Potatoes', description: 'Better fried', src: "./images/potatoes.jpg", perc_carbs: "75", is_fav: false}
-    ]
+    ],
   }
 });
