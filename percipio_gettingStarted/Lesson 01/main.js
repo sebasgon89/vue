62b9e44f5bb0f3add7c98c541f45027332379247
sebasgon89 <seba3789@gmail.com>
2023-04-06 10:18:14 -0300

    Fixed FA

--- percipio_gettingStarted/Lesson 01/main.js
+++ percipio_gettingStarted/Lesson 01/main.js
@@ -6,7 +6,7 @@ var app = new Vue({
     foods: [
       {name: 'Apple', description: 'Reeed', src:"./images/apple.jpg", perc_carbs: "93", is_fav: true},
       {name: 'Banana', description: 'Yeeellow', src: "./images/banana.jpg", perc_carbs: "80", is_fav: false},
-      {name: 'Eggs', description: 'Just crack them', src: "./images/eggs.jpg", perc_carbs: "5",is_fav: false},
+      {name: 'Eggs', description: 'Just crack them', src: "./images/eggs.jpg", perc_carbs: "5",is_fav: true},
       {name: 'Potatoes', description: 'Better fried', src: "./images/potatoes.jpg", perc_carbs: "75", is_fav: false}
     ]
   }
